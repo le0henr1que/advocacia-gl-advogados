c5fede4cd3422e61a449a72e7f473c9078473064
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-05-14 20:05:12 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/global.css";
+import "../styles/button.css";
+import "../styles/text.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
