7bf1822ce90e7576989af4010188b7436d18a978
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-05-14 20:09:55 -0300

    fix: add conditional ph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+const screen = "PHOTO";
 export default function Home() {
   return (
     <>
+      {screen === "PHOTO" && <div>Photo</div>}
+      {screen !== "PHOTO" && <div>Sem a foto</div>}
       <div>Ola</div>
     </>
   );
